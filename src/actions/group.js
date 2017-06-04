1133961700aaa7d58be25d12ed7fd5208c76b60a
windok <windok.ua@gmail.com>
2017-06-04 13:46:58 +0300

    [Add] fetching groups from translations instead of deprecated API

--- src/actions/group.js
+++ src/actions/group.js
@@ -1,5 +1,6 @@
 import * as actionType from 'Actions';
 import RestClient from 'Services/RestClient';
+import Store from 'Store';
 
 
 export const fetchAll = () => (dispatch) => {
@@ -8,9 +9,16 @@ export const fetchAll = () => (dispatch) => {
     });
 
     // todo iteration if total count is large
-    RestClient.get('/group', {params: {limit: 1000}})
+    RestClient.get('/translation', {params: {limit: 1000, type: 'group'}})
         .then((result) => {
-            const items = result.data.items || [];
+
+            // todo process group in reducer, or consider to immutable js or create objects with type  Group
+            const items = (result.data.items || []).map((groupTranslation) => {
+                return {
+                    id: groupTranslation.id,
+                    name: groupTranslation[Store.getState().storage.user.language] || groupTranslation.en
+                }
+            });
 
             dispatch({
                 type: actionType.FETCH_GROUP_COLLECTION_SUCCESS,
